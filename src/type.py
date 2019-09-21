7d811556c9d209c1453b8f271bf7f3bfa22e4082
starikov_andrey <starikovandru@gmail.com>
2019-09-22 01:02:45 +0300

    TYPE_CLASS correcting

--- src/type.py
+++ src/type.py
@@ -3,34 +3,29 @@ from enum import Enum
 
 
 class TickType():
-    def __init__(self, value: float, timeStamp: datetime.datetime):
-        self.value = value
-        self.timeStamp = timeStamp
+    value: float
+    timeStamp: datetime.datetime
 
 class CandleType():
-    def __init__(self, liveTime: int, input: float, output: float, min: float, max: float, color: str):
-        self.liveTime = liveTime
-        self.input = input
-        self.output = output
-        self.min = min
-        self.max = max
-        self.color = color
-
+    liveTime: int
+    input: float
+    output: float
+    min: float
+    max: float
+    color: str
 
 class TimeRange():
-    def __init__(self, beginTime: datetime.datetime, endTime: datetime.datetime):
-        self.beginTime = beginTime
-        self.endTime = endTime
+    beginTime: datetime.datetime
+    endTime: datetime.datetime
 
 class StatusFlag(Enum):
     outDeal = 0
     inDeal = 1
 
 class Need():
-    def __init__(self, candleCount: int, candleLiveTime: int, dataTimeRange: int):
-        self.candleCount = candleCount
-        self.candleLiveTime = candleLiveTime
-        self.dataTimeRange = dataTimeRange
+    candleCount: int
+    candleLiveTime: int
+    dataTimeRange: int
 
 class Decision(Enum):
     SELL = 0
@@ -38,4 +33,4 @@ class Decision(Enum):
     STAY = 2
 
 def buy():
-    wallet =- 5;
+    wallet =- 5
