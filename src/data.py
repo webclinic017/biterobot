94fd099d07c500a5e07acd7e86335274209323c3
starikov_andrey <starikovandru@gmail.com>
2019-09-24 02:19:28 +0300

    DATA_CLASS documented

--- src/data.py
+++ src/data.py
@@ -5,37 +5,79 @@ import datetime
 
 
 class Data():
-    def __init__(self, timeRange: TimeRange): # Подаем объект datetime с требуемым промежутком времени
+    """Класс данных для обработки тиков
+
+    В классе реализованы функции для работы с данными по сделкам - тиками
+    """
+
+    def __init__(self, timeRange: TimeRange):
+        """Конструктор класса данных
+
+        Args:
+            timeRange: Временной промежуток, на котором работаем с данными
+        """
         self.timeRange = timeRange
 
 
     def getTick(self):
-        self.tick = random.uniform(3000.0, 3300.0)  # Функция, котороя вернет Тик из БД (Димон), пока рандом
+        """Получение тика из БД
+
+        Returns:
+            Текущий тик из БД
+        """
+
+        self.tick = random.uniform(3000.0, 3300.0)  # Вызов ф-ии, котороя вернет Тик из БД, пока рандомное значение
         return self.tick
 
     def fillQueue(self):
+        """Создание очереди тиков
 
-            # Вызов функции Димона с timeRange - преобразуем в строку str
+        Очередь создается на основе тиков из требуемого промежутка времени (см. __init__ -> Arg: timeRange)
+
+        Returns:
+            Требуемую очередь тиков
+        """
+
+        # Вызов функции БД с парметром timeRange(в string?), для инициализации работы с БД
 
         self.queue = []
         self.tickCount = 0
-        while (self.getTick() != None and self.tickCount < 30):  #Когда getTick() вернет None => последний тик в данном диапазоне. Второрое условие - временно для теста (будто у нас 30 тиков)
+        while (self.getTick() != None and self.tickCount < 30):  # Когда getTick() вернет None => последний тик в данном диапазоне. Второрое условие - временно для теста (будто у нас 30 тиков)
             self.queue.append(self.getTick())
             self.tickCount += 1
         return self.queue
 
     def clearQueue(self):
+        """Очищение очереди тиков
+
+        """
+
         self.queue.clear()
 
-        # Потом допилить ф-ии отправки очереди в БД, взятие очереди из БД
-
     def timeCount(self):
+        """Расчет времение из промежутка
+
+        Вычисляется разница между двумя точками на временном промежутке
+
+        Returns:
+            Дельту временного промежутка
+
+        """
+
         delta = self.timeRange.endTime - self.timeRange.beginTime
         return delta
 
     def getTimeRange(self):
+        """Получение временного промежутка
+
+        Returns:
+            Временной промежуток (см. __init__ -> Arg: timeRange)
+        """
+
         return self.timeRange
 
+
+#TESTING#
 
 print("__________DATA_TEST_________ \n")
 
