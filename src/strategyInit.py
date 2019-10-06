93dd0902a770d028c1748b58abe188de6c0e610c
starikov_andrey <starikovandru@gmail.com>
2019-10-07 01:59:08 +0300

    Documented STRATGY_INIT class

--- src/strategyInit.py
+++ src/strategyInit.py
@@ -6,13 +6,28 @@ import datetime
 
 
 class StrategiInit():
+    """Класс инициализации стратегии
+
+    В классе инициализируется стратегия, собираются данные под требования стратегии,
+     создается объект стратегии, стратегия запускается в работу
+    """
+
     def __init__(self, strategy: Strategy_1):
+        """Класс для работы со свечами
+
+        В классе реализованы функции для работы со свечами
+        """
+
         self.strategy = strategy
 
     def getNeed(self):
+        """Сбор данных под требования стратеги
+
+        """
+
         self.strategyNeed = self.strategy.setNeed()
         if (self.strategyNeed.candleCount != None):
-            pass                                                # Выбор диапозона, создание и заполнение свечей
+            pass                                     # Выбор диапозона, создание и заполнение свечей
         if (self.strategyNeed.dataTimeRange != None):
             print("STRATEGY NEEDS DATA FOR " + str(self.strategyNeed.dataTimeRange) + "MINUTES")
             self.timeRange = TimeRange
@@ -27,10 +42,13 @@ class StrategiInit():
             self.strategy.setPastData(self.data)
 
     def start(self):
+        """Запуск работы стратегии
+
+        """
+
         self.strategy.getDecision()
 
-
-            # Далее либо бесконечный цикл, либо что-то еще
+        # Далее либо бесконечный цикл, либо что-то еще
 
 strat = Strategy_1
 A = StrategiInit(strat)
