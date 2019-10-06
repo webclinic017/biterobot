b2a69f8f6e2e4fccd17c80329e1ae4ee13fe6e0c
starikov_andrey <starikovandru@gmail.com>
2019-10-07 01:42:06 +0300

    Documented PROCCESS class

--- src/process.py
+++ src/process.py
@@ -1,7 +1,21 @@
 import type
 
 class Process:
+    """Класс действий при торговле
+
+    В классе реализованы функции для управления кошелком
+    """
+
     def __init__(self, wallet: float, BTC_Wallet: float, eventPercent: float, lossPercent: float):
+        """Конструктор класса свечей
+
+        Args:
+            wallet: баланс кошелька
+            BTC_Wallet: баланс криптокошелька
+            eventPercent: допустимый процент от кошелька, для сделки
+            lossPercent: максимальный процент потерь
+        """
+
         self.wallet = wallet
         self.USD_Wallet = self.wallet
         self.BTC_Wallet = BTC_Wallet
@@ -9,13 +23,19 @@ class Process:
         self.lossPercent = lossPercent
 
     def buy(self, price: float):
+        """Совершение покупки
+        Args:
+            price: Цена покупки
+
+        """
+
         self.price = price
         self.transactionAmount: float
         if (self.USD_Wallet < (self.wallet * (1 - self.lossPercent))):
-            print("YOU ARE IN OUT OF LIMIT \n")
+            print("YOU ARE IN OUT OF A LIMIT \n")
             return
         elif ((self.eventPercent * self.USD_Wallet) > (self.lossPercent * self.USD_Wallet)):
-            print("NOW EVENT_PERCENT IS BIGGER THAN LOSS_PERCENT \n")
+            print("NOW, A EVENT_PERCENT IS BIGGER THAN A LOSS_PERCENT \n")
         else:
             self.transactionAmount = (self.eventPercent * self.USD_Wallet)
             self.USD_Wallet -= self.transactionAmount
@@ -23,6 +43,12 @@ class Process:
             print("YOU BOUGHT " + self.transactionAmount / self.price + "BTC \n")
 
     def sell(self, price: float):
+        """Совершение продажи
+        Args:
+            price: Цена продажи
+
+        """
+
         self.price = price
         self.transactionAmount: float
         self.USD_Wallet += self.BTC_Wallet * self.price
@@ -30,6 +56,12 @@ class Process:
         self.BTC_Wallet = 0
 
     def checkWallet(self, currency: str):
+        """Провекра состояния кошелька
+        Args:
+            currency: Текущий курс
+
+        """
+
         self.currency = currency
         if (self.currency == "BTC"):
             print("YOU HAVE " + self.BTC_Wallet + "BTC \n")
