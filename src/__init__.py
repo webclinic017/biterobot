52000b5d1ac1ebaac8f4c9afc82a908b6475303b
kethavel <kethavel.me@gmail.com>
2020-04-20 15:00:06 +0300

    __init__.py now works

--- src/__init__.py
+++ src/__init__.py
@@ -3,20 +3,20 @@ from backtest import Backtest
 from testStrategy import OnlySellStrategy
 from userStrategy import ExampleStrategy
 from type import TimeRange
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     print('BitBot starts')
-    try:
-        # db = Database("mssql", "localhost", "BitBot", "user", "password")
-        db = Database("sqlite", "", "../resources/db/sqlite3/bitbot_sqlalchemytest2.db")
-        tester = Backtest(OnlySellStrategy(), TimeRange(datetime(2016, 5, 5, 0, 0, 0), datetime(2016, 5, 5, 7, 30, 0)),
-                          1000.0, 0.0, db)
-        result = tester.startTest()
-        for item in result:
-            print(item + ':  ' + str(result[item]))
-    except Exception as exc:
-        print("Something went wrong")
-        print(exc)
-        exit()
+    # db = Database("mssql", "localhost", "BitBot", "user", "password")
+    db = Database("sqlite", "", "../resources/db/sqlite3/bitbot_sqlalchemytest2.db")
+    tester = Backtest(ExampleStrategy(),
+                      TimeRange(datetime(2016, 1, 1, 0, 0, 0, 0) + timedelta(minutes=10) - timedelta(hours=3),
+                                datetime(2016, 1, 1, 2, 0, 0, 0) - timedelta(hours=3)),
+                      'bitmex', 'BTC/USD', 1000.0, 0.0, 3, db)
+    result = tester.startTest()
+    for item in result:
+        print(item + ':  ' + str(result[item]))
+        #  except Exception as exc:
+        # print("Something went wrong")
+    #    print(exc)
+    #    exit()
--- src/testStrategy.py
+++ src/testStrategy.py
@@ -1,19 +1,28 @@
 import random
+from typing import Union
+
+from type import Strategy, TickType, CandleType
 
 
-class OnlySellStrategy:
+class OnlySellStrategy(Strategy):
+    def prepareForBacktest(self, currentTick: Union[TickType, CandleType]) -> int:
+        pass
+
     def __init__(self):
         self.eventPercent = 1
         self.lossPercent = 1
 
     def getDecision(self, currentData):
-        return "SELL"
+        return "SELL", currentData.price * 1.01
 
 
-class BrokenStrategy:
+class BrokenStrategy(Strategy):
+    def prepareForBacktest(self, currentTick: Union[TickType, CandleType]) -> int:
+        pass
+
     def __init__(self):
         self.eventPercent = 1
         self.lossPercent = 1
 
     def getDecision(self, currentData):
-        return "i am broken"
+        return "i am broken", currentData.price * 1.01
