90fffd0297b33b7b40e27c97f113ba573c741730
drzenit <starikovandru@gmail.com>
2020-11-12 01:40:54 +0300

    add StrategyModel

--- strategyManager/models.py
+++ strategyManager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ("RUNNING", "RUNNING"),
+    ("PAUSED", "PAUSED"),
+    ("STOPPED", "STOPPED"),
+)
+
+
+class StrategyModel(models.Model):
+    name = models.CharField(max_length=200)
+    version = models.IntegerField(default=1)
+    description = models.CharField(max_length=1000)
+    filePath = models.FilePathField()
+    #strategyTest = models.ForeignKey()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="STOPPED")
+
