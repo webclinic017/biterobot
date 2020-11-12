edd07f27d7521ab812ed2420e62ecec865905e7a
drzenit <starikovandru@gmail.com>
2020-11-12 23:21:11 +0300

    fixes for correct work

--- biterobot/strategyManager/models.py
+++ biterobot/strategyManager/models.py
@@ -12,7 +12,9 @@ class StrategyModel(models.Model):
     name = models.CharField(max_length=200)
     version = models.IntegerField(default=1)
     description = models.CharField(max_length=1000)
-    filePath = models.FilePathField()
+    filePath = models.FilePathField(path="C:\\Users\\uzer\PycharmProjects\\biterobot\\biterobot\strategyManager\strategies")
     #strategyTest = models.ForeignKey()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="STOPPED")
 
+    def __str__(self):
+        return self.name
