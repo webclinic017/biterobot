995d9ef48d26f2a5779540ae5e7994ab43a504e3
drzenit <starikovandru@gmail.com>
2020-11-19 16:40:47 +0300

    IT'S WORK!!!

--- biterobot/biterobot/settings.py
+++ biterobot/biterobot/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'strategyManager.apps.StrategymanagerConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'biterobot.urls'
 
