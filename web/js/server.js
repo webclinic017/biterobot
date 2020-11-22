c32b15ff52fe7518396655756debb011783d1e20
Khmyrov Igor <khmyrov.iv@gmail.com>
2020-11-22 18:54:03 +0300

    Removed unnecessary code form data insert

--- web/js/server.js
+++ web/js/server.js
@@ -291,16 +291,6 @@ function updateDataTable(data) {
         newValue = document.createTextNode(item.dtEnd);
         newCell.appendChild(newValue);
     });
-    row=document.createElement("tr");
-    cell1 = document.createElement("td");
-    cell2 = document.createElement("td");
-    textnode1=document.createTextNode(content);
-    textnode2=document.createTextNode(morecontent);
-    cell1.appendChild(textnode1);
-    cell2.appendChild(textnode2);
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    tabBody.appendChild(row);
 }
 
 /******************************************************************
