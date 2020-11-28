3a14c059f2fd76463c45e3fc5f205e55888c168e
drzenit <starikovandru@gmail.com>
2020-11-28 23:28:37 +0300

    add financial InstrumentModel

--- biterobot/dataManager/models.py
+++ biterobot/dataManager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+TYPE_CHOICES = (
+    ("BOND", "BOND"),
+    ("SHARE", "SHARE"),
+)
+
+
+class InstrumentModel(models.Model):
+    name = models.CharField(max_length=200)
+    ticker = models.CharField(max_length=100)
+    figi = models.CharField(max_length=150)
+    instrumentType = models.CharField(max_length=20, choices=TYPE_CHOICES, default="BOND")
+    isin = models.CharField(max_length=150)
+    minPriceIncrement = models.FloatField()
+    lot = models.IntegerField()
+    minQuantity = models.IntegerField()
+    currency = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
