35da680716adda65e258a3a6bc954702b4d7b753
drzenit <starikovandru@gmail.com>
2020-11-30 01:16:15 +0300

    add models in Django admin

--- biterobot/dataManager/admin.py
+++ biterobot/dataManager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import InstrumentModel, CandleModel
+
+
+admin.site.register(InstrumentModel)
+admin.site.register(CandleModel)
