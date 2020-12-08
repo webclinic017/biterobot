f921078dddcab7119ff5d0bd700e80dec9ceacc0
drzenit <starikovandru@gmail.com>
2020-12-08 23:35:17 +0300

    fix InstrumentModel

--- biterobot/dataManager/models.py
+++ biterobot/dataManager/models.py
@@ -45,7 +45,7 @@ class InstrumentModel(models.Model):
     minPriceIncrement = models.FloatField()  # Минимальная цена
     lot = models.IntegerField()  # Минимальный лот
     minQuantity = models.IntegerField()  # Минимальное количество
-    currency = models.CharField(max_length=50)  # Валюта
+    currency = models.CharField(max_length=20, choices=CURRENCY_CHOICES, default=CURRENCY_USD)  # Валюта
 
     def __str__(self):
         return self.name
