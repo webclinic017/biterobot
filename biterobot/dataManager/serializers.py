0f0a872474c741d0a8ec64c138e2d0ea93a5622d
drzenit <starikovandru@gmail.com>
2020-12-09 00:04:10 +0300

    ready InstrumentSerializerGET serializer

--- biterobot/dataManager/serializers.py
+++ biterobot/dataManager/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 
 class InstrumentSerializerGET(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
     ticker = serializers.CharField(max_length=100)  # Сокращенное название инструмента
     dateBegin = serializers.DateTimeField()
     dateEnd = serializers.DateTimeField()
+    timeFrame = serializers.DateTimeField()  # Интервал свечи (5 минут, 15 минут, день и т.д.)
