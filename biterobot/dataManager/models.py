6e821bc73f1d1af9f9215e3aa1053292052673fa
drzenit <starikovandru@gmail.com>
2020-12-08 23:33:03 +0300

    some fixes

--- biterobot/dataManager/models.py
+++ biterobot/dataManager/models.py
@@ -68,14 +68,12 @@ class DataIntervalModel(models.Model):
     timeFrame = models.DateTimeField()  # Интервал свечи (5 минут, 15 минут, день и т.д.)
 
 class CandleModel(models.Model):
-    # TODO: а здесь нужен FK на инструмент?
     instrument = models.ForeignKey(InstrumentModel, on_delete=models.CASCADE)
-    dataInteraval = models.ForeignKey(DataIntervalModel, on_delete=models.CASCADE)
-    # TODO: в timeframe должен быть стринг с выборами из переменных TinkoffAPI
-    timeFrame = models.DateTimeField()  # интервал свечи (5 минут, 15 минут, день и т.д.)
+    dataInterval = models.ForeignKey(DataIntervalModel, on_delete=models.CASCADE)
+    timeFrame = models.DateTimeField()  # Интервал свечи (5 минут, 15 минут, день и т.д.)
     o = models.FloatField()  # Открытие свечи
     c = models.FloatField()  # Закрытие свечи
     h = models.FloatField()  # Высший уровень свечи - хвост
     l = models.FloatField()  # Низший уровень свечи - хвост
     v = models.IntegerField()  # Объем торгов
-    candleTime = models.DateTimeField()  # Dремя начала свечи в формате datetime
+    candleTime = models.DateTimeField()  # Время начала свечи в формате datetime
