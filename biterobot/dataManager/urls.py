bd170657c143df7a94457a9bfd4f6e8e69500604
drzenit <starikovandru@gmail.com>
2020-12-12 12:24:14 +0300

    DELETE data works now!

--- biterobot/dataManager/urls.py
+++ biterobot/dataManager/urls.py
@@ -6,5 +6,5 @@ from .views import startPage, InstrumentView
 urlpatterns = [
     path('', startPage),
     path('instruments/', InstrumentView.as_view()),
-    path('instruments/<int:pk>/$/', InstrumentView.as_view()),
+    path('instruments/<int:pk>/', InstrumentView.as_view()),
 ]
