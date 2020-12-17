da746cc44c6ffc6bbdddab917618ccb756898bfe
drzenit <starikovandru@gmail.com>
2020-12-18 00:17:02 +0300

    delete wrapper test module

--- biterobot/testManager/common.py
+++ biterobot/testManager/common.py
@@ -1,10 +1,9 @@
-from .backtest import wrapper
 import importlib.util
 
 def testInit(strategyClass):
     spec = importlib.util.spec_from_file_location("TestStrategy",
-                                                  "C:\\Users\\uzer\Desktop\BitBot-Технологии разработки\TestStrategy.py")
+                                                  "path")
     foo = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(foo)
 
-    wrapper.strategyHandler(foo.TestStrategy)
+    #wrapper.strategyHandler(foo.TestStrategy)
