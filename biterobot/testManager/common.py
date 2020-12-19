ce2bdbc8530a50c827ac4508ffe36ceeae397ace
drzenit <starikovandru@gmail.com>
2020-12-20 00:05:44 +0300

    start dataFrame functional

--- biterobot/testManager/common.py
+++ biterobot/testManager/common.py
@@ -1,13 +1,18 @@
 import importlib.util
 
 from .backtest import manager
+from dataManager.models import DataIntervalModel, CandleModel
 
 
-def testInit(strategyPath, taskId):
+def testInit(taskId, strategyPath, dateBegin, dateEnd, ticker, candleLength):
+    # Получение класса стратегии
     spec = importlib.util.spec_from_file_location("TestStrategy",
                                                   strategyPath)
     foo = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(foo)
+
+    # Получение данных Candle и перевод в DF
+    createDF(dateBegin=dateBegin, dateEnd=dateEnd, ticker=ticker, candleLength=candleLength)
 
     print("STRATEGY CLASS - ", foo.TestStrategy)  # Печать класса для теста работы
     print("TASK ID - ", taskId)
@@ -15,3 +20,9 @@ def testInit(strategyPath, taskId):
     backtest = manager.BacktestManager()
 
     backtest.createTask(taskId, foo.TestStrategy)
+
+def createDF(dateBegin, dateEnd, ticker, candleLength):
+    dataIntervalId = DataIntervalModel.objects.get(dateBegin=dateBegin, dateEnd=dateEnd, ticker=ticker, candleLength=candleLength)
+    #print(dataIntervalId)
+
+
