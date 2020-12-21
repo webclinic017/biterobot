e4b27c74c95bc6a8e4c1b93b2119bff8c8e58dbb
g.ivanov <g.ivanov@okko.tv>
2020-12-21 04:33:47 +0300

    bug fixed, test added

--- biterobot/testManager/backtest/backtraderWrapper.py
+++ biterobot/testManager/backtest/backtraderWrapper.py
@@ -13,21 +13,48 @@ from pandas import DataFrame
 from testManager.backtest.const import taskStatus
 
 
-def runBacktestParallel(cerebro: bt.Cerebro, strategyFilePath: str, result: dict, plotFilePath: str):
-    spec = importlib.util.spec_from_file_location("strategy_module", strategyFilePath)
-    foo = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(foo)
-    sys.modules["strategy_module"] = foo
-    cerebro.addstrategy(foo.Strategy)
+def runBacktestParallel(init: dict, result: dict) -> None:
+    """
+    Function to run backtest. Using with multiprocessing
+    :param init: dict with initial parameters
+    :param result: dict with results to return
+    :return: None
+    """
+    # load strategy type
+    spec = importlib.util.spec_from_file_location("strategy_module", init['strategyFilePath'])
+    strategy_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(strategy_module)
+    sys.modules["strategy_module"] = strategy_module
+
+    # create backtrader.cerebro
+    # init cerebro
+    cerebro = bt.Cerebro()
+    # add strategy
+    cerebro.addstrategy(strategy_module.Strategy)
+    # add data
+    cerebro.adddata(init['data'])
+    # define start cash
+    cerebro.broker.setcash(init['startCash'])
+    # Set the commission
+    cerebro.broker.setcommission(commission=init['comission'])
+
+    # define output to redirect stdout and return it
     output = io.StringIO()
-#    result['startCash'] = cerebro.broker.getvalue()
+    # start portfolio value
+    result['startCash'] = cerebro.broker.getvalue()
+    # redirect stdout
     with contextlib.redirect_stdout(output):
+        # run backtest
         cerebro.run()
-        # b = Bokeh(style='bar', output_mode='save', filename=plotFilePath)
-        # cerebro.plot(b)
+        # create plot
+        b = Bokeh(style='bar', output_mode='save', filename=init['plotFilePath'])
+        cerebro.plot(b)
+    # end portfolio value
+    result['endCash'] = cerebro.broker.getvalue()
+    # go to beginning of the output
     output.seek(0)
-    # result['endCash'] = cerebro.broker.getvalue()
-    # result['output'] = output.getvalue()
+    # save output
+    result['output'] = output.getvalue()
     output.close()
 
 
@@ -35,14 +62,12 @@ class Wrapper:
     def __init__(self, strategyFilePath: str, data: DataFrame, plotFilePath: str,
                  startCash: int = 1000, commission: float = 0):
         # attributes for backtrader
-        # spec = importlib.util.spec_from_file_location("strategy_module", strategyFilePath)
-        # foo = importlib.util.module_from_spec(spec)
-        # spec.loader.exec_module(foo)
-        self.strategyFilePath: str = strategyFilePath
-        self.data: bt.feeds.PandasData = bt.feeds.PandasDirectData(dataname=data)
-        self.startCash: int = startCash
-        self.comission: float = commission
-        self.plotFilePath: str = plotFilePath
+        self.init = dict()
+        self.init['strategyFilePath']: str = strategyFilePath
+        self.init['data']: bt.feeds.PandasData = bt.feeds.PandasDirectData(dataname=data)
+        self.init['startCash']: int = startCash
+        self.init['comission']: float = commission
+        self.init['plotFilePath']: str = plotFilePath
 
         # attributes for manage process
         self.process: Union[Process, None] = None
@@ -51,25 +76,13 @@ class Wrapper:
         self.targetToRun: Callable
         self.status = taskStatus.CREATED
 
-        # init cerebro
-        self.cerebro: bt.Cerebro = bt.Cerebro()
-        # add strategy
-        # self.cerebro.addstrategy(self.strategy)
-        # add data
-        self.cerebro.adddata(self.data)
-        # define start cash
-        self.cerebro.broker.setcash(self.startCash)
-        # Set the commission
-        self.cerebro.broker.setcommission(commission=self.comission)
-
     def run(self):
         """Run backtest"""
 
         # check if thread is already exists
         if self.process is None:
             # create if so
-            self.process = Process(target=runBacktestParallel, args=(self.cerebro, self.strategyFilePath,
-                                                                     self.result, self.plotFilePath))
+            self.process = Process(target=runBacktestParallel, args=(self.init, self.result))
         # run
         self.process.start()
         # change status
@@ -102,7 +115,7 @@ class Wrapper:
         :return: result of backtesting
         """
         if self.getStatus() == taskStatus.DONE:
-            return self.result['startCash'], self.result['endCash'], self.result['output'], self.plotFilePath
+            return self.result['startCash'], self.result['endCash'], self.result['output'], self.init['plotFilePath']
         else:
             return None
 
@@ -111,7 +124,3 @@ class Wrapper:
 
     def stop(self):
         pass
-
-if __name__ == '__main__':
-    runBacktestParallel(None, "C:/Kethavel/Projects/BiteRobot/biterobot/testManager/backtest/tests/folder"
-                                   "/TestStrategy.py", None, None)
--- biterobot/testManager/backtest/tests/test_manager.py
+++ biterobot/testManager/backtest/tests/test_manager.py
@@ -1,10 +1,15 @@
-import sys
+import os
 
 from pandas import read_csv
 
 from testManager.backtest.manager import BacktestManager
+from testManager.backtest.const import taskStatus
 
 import unittest
+
+CURRENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEST_STRATEGY_PATH = os.path.join(CURRENT_DIR, "tests/folder/TestStrategy.py")
+PLOT_PATH = os.path.join(CURRENT_DIR, "tests/folder/plot.html")
 
 
 class TestTestManager(unittest.TestCase):
@@ -16,12 +21,14 @@ class TestTestManager(unittest.TestCase):
     def test_createTask(self):
         manager = BacktestManager()
         taskId = 1
-        manager.createTask(taskId, "./folder/TestStrategy.py", self.dataframe, './plot.html')
+        manager.createTask(taskId, TEST_STRATEGY_PATH, self.dataframe, PLOT_PATH)
 
     def test_run(self):
         manager = BacktestManager()
         taskId = 1
-        manager.createTask(taskId, "C:/Kethavel/Projects/BiteRobot/biterobot/testManager/backtest/tests/folder"
-                                   "/TestStrategy.py", self.dataframe, "C:/Kethavel/Projects/BiteRobot/biterobot/testManager/backtest/tests/folder"
-                                   "/plot.html")
+        manager.createTask(taskId, TEST_STRATEGY_PATH, self.dataframe, PLOT_PATH)
         manager.run(1)
+        manager.tasks[1].process.join()
+        assert manager.getStatus(1) == taskStatus.DONE
+        assert manager.getResult(1) is not None
+
